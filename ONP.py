dc197d56417e6f83729254d63ee0a5f74d5e429c
7u7anna <77678424+7u7anna@users.noreply.github.com>
2021-03-04 16:58:22 +0100

    Add files via upload

---
+++ ONP.py
@@ -0,0 +1,41 @@
+#Napisz algorytm , ktory wyznaczy wartosc wyrazenia zapisanego w ONP
+
+s = str(input())
+T = []
+def algorytmONP(T, s, a):
+    n = len(s)
+    for i in range(n):
+        if s[i].isdigit():
+           T.append(s[i])
+        else:
+            if s[i] == '+':
+                y = T[-2]
+                x = T[-1]
+                count = int(T[-2]) + int(T[-1])
+                T.remove(x)
+                T.remove(y)
+                T.append(count)
+            if s[i] == '-':
+                y = T[-2]
+                x = T[-1]
+                count1 = int(T[-2]) - int(T[-1])
+                T.remove(x)
+                T.remove(y)
+                T.append(count1)
+            if s[i] == '*':
+                y = T[-2]
+                x = T[-1]
+                count2 = int(T[-2]) * int(T[-1])
+                T.remove(x)
+                T.remove(y)
+                T.append(count2)
+            if s[i] == '/':
+                y = T[-2]
+                x = T[-1]
+                count3 = int(T[-2]) / int(T[-1])
+                T.remove(x)
+                T.remove(y)
+                T.append(count3)        
+a = " ".join(T)     
+algorytmONP(T, s, a)
+print(a)
